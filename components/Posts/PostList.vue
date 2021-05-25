972af2e880af99caf97ea4d66a3bec2e12074e38
Viktor Palyuha <viktorpalyuha@gmail.com>
2021-05-26 00:36:08 +0300

    pages, routing implemented

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="post-list">
+    <PostPreview
+      id="1"
+      :is-admin="isAdmin"
+      thumbnail="https://iotip.tech/img/iotip/hard.png"
+      title="Hello there!"
+      previewText="This is my first post!"
+    />
+  </section>
+</template>
+
+<script>
+import PostPreview from "~/components/Posts/PostPreview";
+
+export default {
+  components: {
+    PostPreview
+  },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
